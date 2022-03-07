858ece9ba20fe521276f2f88e99acafee3bf8096
rily53 <hori.ri77@gmail.com>
2022-03-08 01:18:19 +0900

    ⑤商品出品機能-バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,6 +20,11 @@ class Item < ApplicationRecord
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" }
 
-  validates :price, presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } do
+    validates :price
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
+
 
 end
