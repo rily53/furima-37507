e794db19b6090be95788c7a0ad7a01cdcc6c8709
rily53 <hori.ri77@gmail.com>
2022-03-07 23:29:30 +0900

    ⑤商品出品機能-DB保存＆エラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -22,7 +23,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :status_id, :shipping_charge_id, :area_id, :shipping_day_id, :price)
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :status_id, :shipping_charge_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validate :user_id
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
@@ -9,11 +14,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :shipping_day
 
-  validates :item_name, :string, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" }
+
+  validates :price, presence: true
 
 end
