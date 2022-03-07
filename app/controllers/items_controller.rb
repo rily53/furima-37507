eb4d96e8e952e723e86122fa5a7089b2af1dea9d
rily53 <hori.ri77@gmail.com>
2022-03-07 14:49:24 +0900

    Revert "⑤商品出品機能-ActiveHash＆画像投稿機能準備"
    This reverts commit 514b5793d08f7b8f38c3dcf4991bc7455943ed78.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,22 +7,9 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
-    @item = Item.new
   end 
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :status_id, :shipping_charge_id, :area_id, :shipping_day_id, :price)
   end
 
 end
