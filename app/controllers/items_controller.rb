cb3385c61598e60e72d7eb8d025d71d6e9ee4ab3
rily53 <hori.ri77@gmail.com>
2022-03-09 01:48:35 +0900

    ⑥商品一覧表示機能-Rubocop実行＆コメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.order("created_at DESC")
-    # @items = nil
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
