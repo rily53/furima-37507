43aef5194090d725ae650bcf281ae2f6a4aa62b8
rily53 <hori.ri77@gmail.com>
2022-03-12 01:20:29 +0900

    ⑩商品購入機能-購入機能設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,12 +1,19 @@
 class BuyersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
-
   def index
     @buyer_address = BuyerShippingAddress.new
   end
 
   def create
+    @buyer_address = BuyerShippingAddress.new(buyer_params)
+    if @buyer_address.valid?
+      pay_item
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
@@ -15,5 +22,19 @@ class BuyersController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def buyer_params
+    params.require(:buyer_shipping_address).permit(:post_code, :area_id, :municipalities, :address, :building_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 
 end
--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -1,23 +1,17 @@
 class BuyerShippingAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :municipalities, :address, :building_name, :phone, :user_id, :item_id, :buyer_id
+  attr_accessor :post_code, :area_id, :municipalities, :address, :building_name, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :address
     validates :phone
     validates :user_id
     validates :item_id
-    validates :buyer_id
+    validates :token
   end
-
-  # validates :building_name
-
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :area
-
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
