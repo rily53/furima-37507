ce1be8abfa87e1684db7fa5085c152759539ff75
rily53 <hori.ri77@gmail.com>
2022-03-12 11:33:09 +0900

    ⑩商品購入機能-バリデーション修正

--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -3,15 +3,17 @@ class BuyerShippingAddress
   attr_accessor :post_code, :area_id, :municipalities, :address, :building_name, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :post_code
+    validates :token
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :address
-    validates :phone
+    validates :phone, length: { is: 11, message: 'is too short' }
     validates :user_id
     validates :item_id
-    validates :token
   end
+
+    validates :phone, format: { with: /\d{10,11}/, message: 'is invalid. Input only number' }
 
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
